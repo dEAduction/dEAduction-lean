f5cd22cda23aa190bae8f5752475c2a9250e5c69
Frédéric Le Roux <frederic.le-roux@imj-prg.fr>
2020-07-28 17:10:29 +0200

    snippets: exercises_theories_des_ensembles

--- src/snippets/exercises/exercises_theorie_des_ensembles.lean
+++ src/snippets/exercises/exercises_theorie_des_ensembles.lean
@@ -11,118 +11,113 @@ import structures
 local attribute [instance] classical.prop_decidable
 
 
---------------------------------
--- Commentaire deaduction:
--- le logiciel parse les sections (*todo*) et les définitions
--- on peut définir des liste (macros) dans des docstrings, syntaxe
--- @Macro
--- suivi du nom de la macro sur une ligne seule,
--- suive de la liste qui finit à la fin du docstring
--- The lemmas that will serve as exercises are tagged by @Exercise
--- followed by the title on a single line
--- followed by a text description preceded by @Description
--- followed by macros representing the list of active buttons in each window
 
--- pendant les exos qui suivent, le logiciel affichera en titre : 
--- Théorie des ensembles (1) Ensembles
--- Puis, plus loin,
--- Théorie des ensembles (2) Applications
+lemma definition.iff {P Q : Prop} : ( P ↔ Q ) ↔ (P → Q) ∧ (Q → P) := 
+iff_def
 
+namespace theorie_des_ensembles
 
+variable {X : Type}
 
+lemma definition.inclusion (A B : set X) : A ⊆ B ↔ ∀ {{x:X}}, x ∈ A → x ∈ B :=
+iff.rfl
 
-
-
-/- 
-@Macro
-STANDARD_LOGIC
-∀, ∃, →, ↔, ET, OU, NON, 
-Absurde, Contraposée, Par cas, Choix
+lemma definition.egalite_deux_ensembles {A A' : set X} : (A = A') ↔ ( ∀ x, x ∈ A ↔ x ∈ A' ) :=
+/- dEAduction
+PrettyName
+    egalité de deux ensembles
 -/
+by exact set.ext_iff
 
-
--- CDD : les deux lignes qui suivent indiquent les boutons à intégrer dans les zones correspondantes
--- pour tous les exercices de la section
--- @Definitions theorie_des_ensembles
--- (inclure toutes les définitions de la section "théorie des ensemble",
--- i.e. celles dont le nom commence par "definitions.theorie_des_ensembles")
--- 
-
------------------------------------------
------------------------------------------
-section unions_et_intersections  -- sous-section 1
------------------------------------------
------------------------------------------
-namespace definitions.unions_et_intersections
-
-
-
-
-
-variables {X : Type} {A B C : set X}
-
-/- 
-@Exercise
-Intersection d'unions
-@Description
-L'intersection est distributive par rapport à l'union
-@Macro
-LOGIC
-STANDARD_LOGIC +Contradiction -Choix
--- @Macro
--- DEFINITIONS
--- @Macro
--- THEOREM
-@ExpectedVariables
-2
+lemma theorem.double_inclusion {A A' : set X} : (A ⊆ A' ∧ A' ⊆ A) → A = A' :=
+/- dEAduction
+PrettyName
+    Double inclusion
 -/
-lemma union_distributive_inter : A ∩ (B ∪ C)  = (A ∩ B) ∪ (A ∩ C) := 
 begin
-    defi double_inclusion, ET,
-    defi inclusion, qqs a, implique,
-    hypo_analysis,
-    goals_analysis,
-    defi intersection_deux at H,
-    hypo_analysis,
-    goals_analysis,
-    ET H,
-    defi union at HB,
-    OU HB,
-      defi union, OU gauche,
-      ET HA HA_1,
-      defi intersection_deux at H,
-      assumption,
-    defi union, OU droite,
-    defi intersection_deux, ET, assumption, assumption,
-  defi inclusion, qqs a, implique,
-  defi union at H,
-  OU H,
-    defi intersection_deux at HA,
-    ET HA,
-    OUd HB a ∈ C,  -- évitable en travaillant sur le but
-    defi union at H,
-    ET HA_1 H,
-  defi intersection_deux at H_1,
-  assumption,
-  defi intersection_deux at HB,
-  ET HB,
-  defi intersection_deux,
-  ET,
-  assumption,
-  defi union,
-  tautology,
+    exact set.subset.antisymm_iff.mpr
 end
 
-/- 
-@Exercise
-Union d'intersections
-@Description
-L'union est distributive par rapport à l'intersection 
+
+-----------------------------------------
+--------------- section 1 ---------------
+namespace unions_et_intersections
+-----------------------------------------
+-----------------------------------------
+
+variables {A B C : set X}
+
+
+lemma definition.intersection_deux_ensembles (A B : set X) (x : X) :  x ∈ A ∩ B ↔ ( x ∈ A ∧ x ∈ B) :=
+iff.rfl
+/- dEAduction
+PrettyName
+    Intersection de deux ensembles
 -/
-lemma inter_distributive_union : A ∪ (B ∩ C)  = (A ∪ B) ∩ (A ∪ C) := 
+
+lemma definition.intersection_quelconque_ensembles (I : Type) (O : I → set X)  (x : X) : (x ∈ set.Inter O) ↔ (∀ i:I, x ∈ O i) :=
+set.mem_Inter
+/- dEAduction
+PrettyName
+    Intersection d'une famille d'ensembles quelconque
+-/
+
+lemma definition.union_deux_ensembles  (A : set X) (B : set X) (x : X) :  x ∈ A ∪ B ↔ ( x ∈ A ∨ x ∈ B) :=
+iff.rfl
+/- dEAduction
+PrettyName
+    Union de deux ensembles
+-/
+
+lemma definition.union_quelconque_ensembles (I : Type) (O : I → set X)  (x : X) : (x ∈ set.Union O) ↔ (∃ i:I, x ∈ O i) :=
+set.mem_Union
+/- dEAduction
+PrettyName
+    Union d'une famille d'ensembles quelconque
+-/
+
+
+
+lemma exercise.union_distributive_inter : A ∩ (B ∪ C)  = (A ∩ B) ∪ (A ∩ C) := 
+/- dEAduction
+PrettyName
+    Intersection d'une union
+Description
+    L'intersection est distributive par rapport à l'union
+Tools->Logic
+    $ALL -negate
+Tools->ProofTechniques
+    $ALL -contradiction
+Tools->Definitions
+    $UNTIL_NOW
+Tools->Theorems
+    double_inclusion
+ExpectedVarsNumber
+    X=3, A=1, B=1
+-/
+
 begin
-    hypo_analysis,
-    goals_analysis,
+    sorry
+end
+
+set_option pp.all
+lemma exercise.inter_distributive_union : A ∪ (B ∩ C)  = (A ∪ B) ∩ (A ∪ C) := 
+/- dEAduction
+PrettyName
+    
+Description
+    
+Tools->Logic
+    $ALL
+Tools->ProofTechniques
+    $ALL
+Tools->Definitions
+    $UNTIL_NOW
+Tools->Theorems
+    $ALL   
+-/
+
+begin
     sorry
 end
 
@@ -133,71 +128,131 @@ end unions_et_intersections
 section complementaire -- sous-section 2
 -----------------------------------------
 -----------------------------------------
-variables {X : Type} {A B : set X}
+variables  {A B : set X}
 variables {I : Type} {E F : I → set X}
--- notation X `\` A := @has_neg.neg (set X) _ A
--- notation  ` \` Z := has_neg.neg Z   
--- notation `\ ` Z := -Z
+notation A`ᶜ` := set.compl A 
 
-/- 
-@Exercise
-Complémentaire du complémentaire
-@Description
-Tout ensemble est égal au complémentaire de son complémentaire
+lemma definition.complement {A : set X} {x : X} : x ∈ set.compl A ↔ x ∉ A := 
+/- dEAduction
+PrettyName
+    Complémentaire
 -/
-lemma complement_complement : - - A =A :=
+by finish
+
+lemma definition.difference_symetrique {A B : set X} {x : X} : x ∈ B \ A ↔ (x ∈ B ∧ x ∉ A) :=
+/- dEAduction
+PrettyName
+    Différence symétrique
+-/
+iff.rfl
+
+
+lemma complement_complement : (set.compl (set.compl A)) =A :=
+/- dEAduction
+PrettyName
+    Complémentaire du complémentaire    
+Description
+    Tout ensemble est égal au complémentaire de son complémentaire    
+Tools->Logic
+    $ALL
+Tools->ProofTechniques
+    $ALL
+Tools->Definitions
+    $UNTIL_NOW
+Tools->Theorems
+    $ALL   
+-/
 begin
     hypo_analysis,
+    targets_analysis,
     sorry
 end
 
-/- 
-@Exercise
-Complémentaire d'union I
-@Description
-Le complémentaire de l'union de deux ensembles égale l'intersection des complémentaires 
+lemma complement_union_deux : set.compl (A ∪ B) = (set.compl A) ∩ (set.compl B) :=
+/- dEAduction
+PrettyName
+    Complémentaire d'union I
+Description
+    Le complémentaire de l'union de deux ensembles égale l'intersection des complémentaires 
+Tools->Logic
+    $ALL
+Tools->ProofTechniques
+    $ALL
+Tools->Definitions
+    $UNTIL_NOW
+Tools->Theorems
+    $ALL   
 -/
-lemma complement_union_deux : @has_neg.neg (set X) _ (A ∪ B) = (- A) ∩ (- B) :=
 begin    
-    hypo_analysis,
-    goals_analysis,
     sorry
 end
 
 
 
-/- 
-@Exercise
-Complémentaire d'union II
-@Description
-Le complémentaire d'une réunion quelconque égale l'intersection des complémentaires 
--/
 open set
 -- set_option pp.all true
-lemma complement_union_quelconque  (H : ∀ i, F i = - E i) : - (Union E) = Inter F :=
-
+lemma exercise.complement_union_quelconque  (H : ∀ i, F i = set.compl (E i)) : set.compl (Union E) = Inter F :=
+/- dEAduction
+PrettyName
+    Complémentaire d'union II    
+Description
+    Le complémentaire d'une réunion quelconque égale l'intersection des complémentaires     
+Tools->Logic
+    $ALL
+Tools->ProofTechniques
+    $ALL
+Tools->Definitions
+    $UNTIL_NOW
+Tools->Theorems
+    $ALL   
+-/
 begin
-    hypo_analysis,
-    goals_analysis,
     sorry
 end
 
-/- @EXERCICE Le complémentaire d'une intersection quelconque égale l'union des complémentaires -/
--- set_option pp.all true
-lemma complement_intersection_quelconque  (H : ∀ i, F i = - E i) : - (Inter E) = Union F :=
+lemma complement_intersection_quelconque  (H : ∀ i, F i = set.compl (E i)) : set.compl (Inter E) = Union F :=
+/- dEAduction
+PrettyName
+    
+Description
+    Le complémentaire d'une intersection quelconque égale l'union des complémentaires    
+Tools->Logic
+    $ALL
+Tools->ProofTechniques
+    $ALL
+Tools->Definitions
+    $UNTIL_NOW
+Tools->Theorems
+    $ALL   
+-/
 begin   
     sorry
 end
-/- @EXERCICE Le complémentaire du vide est X ??? -/ 
+
+
+/- Le complémentaire du vide est X ??? -/ 
 
 
 
-/- @EXERCICE A est inclus dans B ssi le complémentaire de A contient le complémentaire de B -/
-lemma inclus_ssi_complement_contient : A ⊆ B ↔ - B ⊆ - A :=
-begin    
-    hypo_analysis,
-    goals_analysis,
-    defi double_implication,    ET, 
+lemma inclus_ssi_complement_contient : A ⊆ B ↔ set.compl B ⊆ set.compl A :=
+/- dEAduction
+PrettyName
+    
+Description
+    A est inclus dans B ssi le complémentaire de A contient le complémentaire de B    
+Tools->Logic
+    $ALL
+Tools->ProofTechniques
+    $ALL
+Tools->Definitions
+    $UNTIL_NOW
+Tools->Theorems
+    $ALL   
+-/
+
+begin
+    defi double_implication,
+        ET, 
         implique,
         defi inclusion, qqs a, implique, 
         defi complement,
@@ -217,11 +272,11 @@ end
 
 
 /- -/
-example : A ⊆ B ↔ B - A = ∅ :=
+example : A ⊆ B ↔ B \ A = ∅ :=
 begin
-    hypo_analysis,
-    goals_analysis,
-    sorry
+    split,
+    intro H,
+
 end
 
 -- Comment manipuler l'ensemble vide dans un type ?
@@ -248,33 +303,85 @@ section applications  -- sous-section 5
 notation f `⟮` A `⟯` := f '' A
 notation f `⁻¹⟮` A `⟯` := f  ⁻¹' A
 
-variables {X : Type} (A A': set X) 
+variables  (A A': set X) 
 variables {Y: Type} {f: X → Y} (B B': set Y)
 variables {I : Type} {E : I → set X} {F : I → set Y}
 
-/- @EXERCICE -/
-lemma image_de_reciproque : f '' (f ⁻¹' B)  ⊆ B :=
+lemma exercise.image_de_reciproque : f '' (f ⁻¹' B)  ⊆ B :=
+/- dEAduction
+PrettyName
+    
+Description
+    
+Tools->Logic
+    $ALL
+Tools->ProofTechniques
+    $ALL
+Tools->Definitions
+    $UNTIL_NOW
+Tools->Theorems
+    $ALL   
+-/
 begin
     hypo_analysis,
     goals_analysis,
     sorry
 end
 
-/- @EXERCICE -/
-lemma reciproque_de_image : A ⊆ f ⁻¹' (f '' A) :=
+lemma exercise.reciproque_de_image : A ⊆ f ⁻¹' (f '' A) :=
+/- dEAduction
+PrettyName
+    
+Description
+    
+Tools->Logic
+    $ALL
+Tools->ProofTechniques
+    $ALL
+Tools->Definitions
+    $UNTIL_NOW
+Tools->Theorems
+    $ALL   
+-/
+begin
+    intros x H,
+end
+
+lemma exercise.image_reciproque_inter :  f ⁻¹'  (B∩B') = f ⁻¹'  (B) ∩ f ⁻¹'  (B') :=
+/- dEAduction
+PrettyName
+    
+Description
+    
+Tools->Logic
+    $ALL
+Tools->ProofTechniques
+    $ALL
+Tools->Definitions
+    $UNTIL_NOW
+Tools->Theorems
+    $ALL   
+-/
 begin
     sorry
 end
 
-/- @EXERCICE -/
-lemma image_reciproque_inter :  f ⁻¹'  (B∩B') = f ⁻¹'  (B) ∩ f ⁻¹'  (B') :=
-begin
-    sorry
-end
 
-
-/- @EXERCICE -/
 lemma  image_reciproque_union  : f ⁻¹' (B ∪ B') = f ⁻¹' B ∪ f ⁻¹' B'  :=
+/- dEAduction
+PrettyName
+    
+Description
+    
+Tools->Logic
+    $ALL
+Tools->ProofTechniques
+    $ALL
+Tools->Definitions
+    $UNTIL_NOW
+Tools->Theorems
+    $ALL   
+-/
 begin
     defi double_inclusion,
     ET,
@@ -282,8 +389,12 @@ begin
         hypo_analysis,
         goals_analysis,
         qqs x,
+        assumption,
         implique,
+        hypo_analysis,
+        goals_analysis,
         defi image_reciproque at H,
+        hypo_analysis,
         defi union,
         defi union at H,
         OU H,
@@ -307,9 +418,24 @@ end
 
 /- Idem union, intersection quelconques -/
 
-/- @EXERCICE -/
 lemma image_reciproque_inter_quelconque  (H : ∀ i:I,  (E i = f ⁻¹' (F i))) :  (f ⁻¹'  (set.Inter F)) = set.Inter E :=
+/- dEAduction
+PrettyName
+    
+Description
+    
+Tools->Logic
+    $ALL
+Tools->ProofTechniques
+    $ALL
+Tools->Definitions
+    $UNTIL_NOW
+Tools->Theorems
+    $ALL   
+-/
 begin
+    hypo_analysis,
+    targets_analysis,
     defi double_inclusion, ET,
     {defi inclusion, qqs x,
     implique,
@@ -332,9 +458,22 @@ end
 
 
 
-/- @EXERCICE -/
 lemma image_inter_inclus_inter_images   :  
         f '' (A∩A') ⊆ f '' (A) ∩ f '' (A') :=
+/- dEAduction
+PrettyName
+    
+Description
+    
+Tools->Logic
+    $ALL
+Tools->ProofTechniques
+    $ALL
+Tools->Definitions
+    $UNTIL_NOW
+Tools->Theorems
+    $ALL   
+-/
 begin
     -- Soit b un élément de  f(A'∩A)
     defi inclusion,
@@ -355,7 +494,7 @@ begin
 end
 
 
-/- @EXERCICE L'image réciproque du complémentaire 
+/- L'image réciproque du complémentaire 
 égale le complémentaire de l'image réciprqque-/
 
 
@@ -365,16 +504,5 @@ end applications
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 end theorie_des_ensembles
+
