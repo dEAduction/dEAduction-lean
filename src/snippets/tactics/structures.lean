d49972bee8208542b264079b2008085ba311be76
Frédéric Le Roux <frederic.le-roux@imj-prg.fr>
2020-07-28 11:10:09 +0200

    feat(structure): add treatment of PROP type (type 0)

--- src/snippets/tactics/structures.lean
+++ src/snippets/tactics/structures.lean
@@ -9,7 +9,7 @@ open lean.parser tactic interactive
 open interactive (loc.ns)
 open interactive.types
 open tactic expr
-local postfix *:9001 := many -- sinon ne comprends pas ident*
+local postfix *:9001 := many -- necessary for ident*
 
 
 def separator_comma := "¿, "
@@ -40,11 +40,13 @@ end
 
 
 
-/- Décompose la racine d'une expression (un seul pas)  -/
-private meta def analyse_expr_step  (e : expr) : tactic (string × (list expr)) := 
+/- Decompose the root of an expression (just one step)  
+BEWARE, propositions should either start by "PROP" or "QUANT"
+    since this is how dEAduction distinguish between objects and props -/
+private meta def analysis_expr_step  (e : expr) : tactic (string × (list expr)) := 
 do  S ←  (tactic.pp e), let e_joli := to_string S, 
 match e with
-------------------------- LOGIQUE -------------------------
+------------------------- LOGIC -------------------------
 | `(%%p ∧ %%q) := return ("PROP_AND", [p,q])
 | `(%%p ∨ %%q) := return ("PROP_OR", [p,q])
 | `(%%p ↔ %%q) := return ("PROP_IFF", [p,q])
@@ -68,7 +70,7 @@ match e with
                     else return ("QUANT_∃", [type, var_, inst_body])
     |  _ := return ("ERROR", [])
     end 
-------------------------- THEORIE DES ENSEMBLES -------------------------
+------------------------- SET THEORY -------------------------
 | `(%%A ∩ %%B) := return ("SET_INTER", [A,B])
 | `(%%A ∪ %%B) := return ("SET_UNION", [A,B])
 | `(set.compl %%A) := return ("SET_COMPLEMENT", [A])
@@ -83,25 +85,16 @@ match e with
 | `(%%f  ⁻¹' %%A) := return ("SET_INVERSE", [f,A])
 | `(∅) := return ("SET_EMPTY", [])
 | `(_root_.set %%X) := return ("SET", [X])
--- polymorphe
-| `(%%a = %%b) := return ("PROP_EQUAL", [a,b]) -- faudrait connaitre le type ?
-| `(%%a ≠ %%b) := return ("PROP_EQUAL_NOT", [a,b]) -- faudrait connaitre le type ?
+-- polymorphic
+| `(%%a = %%b) := return ("PROP_EQUAL", [a,b]) -- does not provide the type
+| `(%%a ≠ %%b) := return ("PROP_EQUAL_NOT", [a,b]) 
 ----------- TOPOLOGY --------------
 -- | `(B(%%x, %%r))
--- | `(0 < %%b) := return ("POSITIF", [b]) 
 | `(%%a < %%b) := return ("PROP_<", [a,b]) 
 | `(%%a ≤ %%b) := return ("PROP_≤", [a,b])
--- | `(%%a > 0) := return ("POSITIF", [a])
 | `(%%a > %%b) := return ("PROP_>", [a,b]) 
 | `(%%a ≥ %%b) := return ("PROP_≥", [a,b]) 
 ------------------------------ Leaves with data ---------------------------
----------------------------- NOMBRES particuliers (cf aussi plus bas) 
---| `(0:ℝ) := return ("NUMBER[0]",[])               -- OK, mais peut-être faut-il garder l'info 0 : réel
---| `(0:ℕ) := return ("NUMBER[0]",[])               -- non testé
---| `(0:ℤ) := return ("NUMBER[0]",[])               -- non testé
---| `(1:ℝ) := return ("NUMBER[1]",[])               
---| `(1:ℕ) := return ("NUMBER[1]",[])               -- non testé
---| `(1:ℤ) := return ("NUMBER[1]",[])               -- non testé
 | (app fonction argument)   := 
     if is_numeral e
         then return ("NUMBER" ++ open_bra ++ e_joli ++ closed_bra, []) 
@@ -112,7 +105,8 @@ match e with
     ++ "name:ℕ" ++ closed_bra,[])
 | (const name list_level)   := return ("CONSTANT" ++ open_bra ++ "name:" 
     ++ e_joli ++ "/" ++ to_string name ++ closed_bra, []) 
-| (sort level)      := return ("TYPE", [])  
+| (sort level.zero) := return ("PROP", [])
+| (sort level) := return ("TYPE", [])  
 | (local_const name pretty_name bi type) := return ("LOCAL_CONSTANT" ++ open_bra 
     ++ "name:" ++ to_string pretty_name ++ "/identifier:" ++ to_string name ++ closed_bra, [type]) 
 ---------------- Useless for dEAduction ? -------------
@@ -124,105 +118,61 @@ match e with
 end
 
 
-/- Analyse récursivement une expression à l'aide de analyse_expr_step, 
-renvoie le résultat sous forme de chaine bien parenthésée-/
-private meta def analyse_rec : expr →  tactic string 
+/- Recursively analysiss an expression using analysis_expr_step, 
+and provides a string with parentheses reflecting the mathematical object-/
+private meta def analysis_rec : expr →  tactic string 
 | e := 
-do ⟨string, liste_expr⟩ ←  analyse_expr_step(e), 
+do ⟨string, liste_expr⟩ ←  analysis_expr_step(e), 
     match liste_expr with
-    -- ATTENTION, cas de plus de trois arguiments non traité
-    -- à remplacer par un list.map
+    -- BEWARE, case of more than three arguments not implemented
+    -- replace by a list.map
     |[e1] :=  do 
-       string1 ← analyse_rec e1,
+       string1 ← analysis_rec e1,
        return(string ++ open_paren ++ string1 ++ closed_paren)
     |[e1,e2] :=  do 
-        string1 ← analyse_rec e1,
-        string2 ← analyse_rec e2,
---        if  string = "APPLICATION"
---            then return (string1 ++ open_paren ++ string2 ++ closed_paren) else
+        string1 ← analysis_rec e1,
+        string2 ← analysis_rec e2,
         return (string ++ open_paren ++ string1 ++ separator_comma ++ string2 ++ closed_paren)
     |[e1,e2,e3] :=  do  
-        string1 ← analyse_rec e1,
-        string2 ← analyse_rec e2,
-        string3 ← analyse_rec e3,
+        string1 ← analysis_rec e1,
+        string2 ← analysis_rec e2,
+        string3 ← analysis_rec e3,
         return (string ++ open_paren ++ string1 ++ separator_comma ++ string2 ++ separator_comma ++ string3 ++ closed_paren)
     | _ :=    return(string)
     end
-private meta def analyse_expr : expr →  tactic string
+
+meta def analysis_expr : expr →  tactic string
 | e := do
     expr_t ←  infer_type e,
     is_pro ← is_prop expr_t,
-    -- expr_tt ← infer_type expr_t,
     if is_pro then do
-            -- S ←  (tactic.pp expr_t), 
-            -- let S1 := to_string S,
             S ←  (tactic.pp expr_t), let et_joli := to_string S, 
-            S1b ← analyse_rec e,
-            S2 ← analyse_rec expr_t,
+            S1b ← analysis_rec e,
+            S2 ← analysis_rec expr_t,
             let S3 := "PROPERTY[" ++ S1b ++ "/pp_type: " ++ et_joli ++ "]" 
                 ++ separator_equal ++ S2,
             return(S3)
         else  do
-            S1b ← analyse_rec e,
-            S2 ← analyse_rec expr_t,
+            S1b ← analysis_rec e,
+            S2 ← analysis_rec expr_t,
             let S3 := "OBJECT[" ++ S1b ++ "]" ++ separator_equal ++ S2,
             return(S3)
 
 
-/- Affiche la liste des objets du contexte, séparés par des retour chariots 
-format :  "OBJET" ou "PROPRIETE" : affichage Lean : structure -/
+/- print a list of strings reflecting objects in the context  -/
 meta def hypo_analysis : tactic unit :=
 do liste_expr ← local_context,
     trace "context:",
-    liste_expr.mmap (λ h, analyse_expr h >>= trace),
+    liste_expr.mmap (λ h, analysis_expr h >>= trace),
     return ()
 
 
-/- Affiche la liste des buts, même format que analyse_contexte
-(excepté qu'il n'y a que des PROPRIETES) -/ 
-meta def goals_analysis : tactic unit :=
+/- print the list of all targets -/ 
+meta def targets_analysis : tactic unit :=
 do liste_expr ← get_goals,
-    trace "goals:", 
-    liste_expr.mmap (λ h, analyse_expr h >>= trace),
+    trace "targets:", 
+    liste_expr.mmap (λ h, analysis_expr h >>= trace),
     return ()
 
-
-
----------------------------------------------------------
---------- NON UTILISES (debuggage) ----------------------------------
----------------------------------------------------------
-
-
-/- Appelle l'analyse récursive sur le but ou sur une hypothèse. Non utilisé par la suite. -/
-meta def analysis (names : parse ident*) : tactic unit := 
-match names with
-    | [] := do goal ← tactic.target,
-                trace (analyse_rec goal)
-    | [nom] := do expr ← get_local nom,
-                expr_t ←  infer_type expr,
-                expr_tt ← infer_type expr_t,
-                -- la suite différencie selon la sémantique, 
-                -- ie les objets (éléments, ensembles, fonctions)
-                -- vs les propriétés
-                if expr_tt = `(Prop) then  
-                    trace (analyse_rec expr_t)
-                else  do S1 ← (analyse_rec expr), 
-                        S2 ← (analyse_rec expr_t),
-                        --let S2 := to_string expr_t,
-                        let S3 := S1 ++ " : "++ S2,
-                        trace(S3)
-    | _ := skip
-    end
-
-/- Appelle l'analyse en 1 coup sur le but ou sur une hypothèse. Non utilisé par la suite. -/
-meta def analysis1 (names : parse ident*) : tactic unit := 
-match names with
-    | [] := do goal ← tactic.target,
-                trace (analyse_expr_step goal)
-    | [nom] := do expr ← get_local nom,
-                expr_t ←  infer_type expr,
-                trace (analyse_expr_step expr_t)
-    | _ := skip
-    end
 
 end tactic.interactive
